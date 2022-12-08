906553f5752730dc6c4088ad545521b48b0b6f98
didier-tp <didier@d-defrance.fr>
2022-12-08 17:13:36 +0100

    s

--- appliSpring/src/main/java/tp/appliSpring/service/CompteServiceImpl.java
+++ appliSpring/src/main/java/tp/appliSpring/service/CompteServiceImpl.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import tp.appliSpring.entity.Compte;
+import tp.appliSpring.entity.Operation;
 import tp.appliSpring.repository.RepositoryCompte;
+import tp.appliSpring.repository.RepositoryOperation;
 
 @Transactional //ou equivalent EJB
 @Service //ou bien @Stateless sur EJB
@@ -13,16 +15,25 @@ public class CompteServiceImpl implements CompteService {
 	
 	@Autowired //ou bien @Inject sur EJB
 	private RepositoryCompte repositoryCompte;
+	
+	@Autowired //ou bien @Inject sur EJB
+	private RepositoryOperation repositoryOperation;
 
 	@Override
 	public void effectuerVirement(double montant, long numCptDeb, long numCptCred) throws RuntimeException {
 		Compte compteDeb = repositoryCompte.findById(numCptDeb);
 		compteDeb.setSolde(compteDeb.getSolde()-montant);
 		repositoryCompte.update(compteDeb);//quelquefois automatique si transaction
+		Operation opDebit = new Operation(null,"debit suite virement",-montant); 
+		opDebit.setCompte(compteDeb);
+		repositoryOperation.insertNew(opDebit);
 		
 		Compte compteCred = repositoryCompte.findById(numCptCred);
 		compteCred.setSolde(compteCred.getSolde()+montant);
 		repositoryCompte.update(compteCred);//quelquefois automatique si transaction
+		Operation opCredit = new Operation(null,"credit suite virement",+montant); 
+		opCredit.setCompte(compteCred);
+		repositoryOperation.insertNew(opCredit);
 	}
 
 }
